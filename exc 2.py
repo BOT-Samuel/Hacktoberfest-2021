270bae5415ebc355c779c2212e6d3438b42f7fd2
Samuel Oliveira <56443581+BOT-Samuel@users.noreply.github.com>
2021-10-30 20:34:22 -0300

    Update exc 2.py

--- exc 2.py
+++ exc 2.py
@@ -3,3 +3,6 @@ exc 2
 Receber do teclado um nome e imprimir tantas vezes
 quantos forem seus caracteres.
 '''
+nome = input("Digite um nome: ")
+quantCaractNome = len(nome)
+print("Seu nome possui",quantCaractNome,"caracteres")
